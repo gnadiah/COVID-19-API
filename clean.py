fde13e7ee4fc90b8cba3495528468fce675c1af4
Nguyen Hai Dang <ndang3399@gmail.com>
2021-08-11 18:22:50 +0700

    Update

--- clean.py
+++ clean.py
@@ -6,8 +6,9 @@ print("Enter the password if the machine asks !!!")
 os_type = platform.system().lower()
 
 if os_type == "darwin" or os_type == "linux" or os_type == "unix":
-    os.system("sudo rm -rf world_data.json vn_data.json")
+    os.system("sudo rm -rf world_data.json vn_data.json __pycache__")
 else:
     os.system("del /f world_data.json vn_data.json")
+    os.system("rmdir /S /Q __pycache__")
 
 print(figlet_format("SUCCESSFULLY"))
